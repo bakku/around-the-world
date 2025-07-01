6b20ee8789eadcc8c10da4be2597fb113cfa585a
Christian Paling <christian.paling@googlemail.com>
2025-07-01 23:02:03 +0200

    Add empty name as a placeholder

--- src/app/_lib/actions.ts
+++ src/app/_lib/actions.ts
@@ -4,6 +4,12 @@ import db from "@/db";
 import { roomsTable } from "@/db/schema";
 
 export async function createRoom() {
-  const result = await db().insert(roomsTable).values({}).returning();
+  const result = await db()
+    .insert(roomsTable)
+    .values({
+      // TODO: Fill this with a proper name.
+      name: "",
+    })
+    .returning();
   return result[0].id;
 }
